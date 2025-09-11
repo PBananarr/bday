2a27957d7ff856b3c87fcdbb47477843c263392a
PBananarr <79418608+PBananarr@users.noreply.github.com>
2025-09-11 10:13:51 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* sw.js — Globaler Service Worker für Birthday-Quest */
 
-const VERSION = "v0.0.1.1.";               // <<< bei jedem Release anpassen
+const VERSION = "v0.0.1.2.";               // <<< bei jedem Release anpassen
 self.VERSION = VERSION;
 const STATIC_CACHE = `bq-static-${VERSION}`;
 const RUNTIME_CACHE = `bq-runtime-${VERSION}`;
@@ -100,5 +100,6 @@ self.addEventListener("fetch", (event) => {
     })());
   }
 });
+
 
 
